79867c51999973e8d3e89190a8533d16794a866a
Chris Bowdon <c.bowdon@bath.edu>
2016-03-25 15:46:47 +0000

    Add initial scripts
    elo - elo ranking functions
    results - getting the data from Ergast

---
+++ elo.fsx
@@ -0,0 +1,40 @@
+open System
+
+let KFactor = 32.0
+
+type Player = { name: string
+                rating: float }
+
+let expectedScore (playerA: Player) (playerB: Player) =
+    1.0 / (1.0 + Math.Pow(10.0, (playerA.rating - playerB.rating) / 400.0))
+
+let updateRating (player: Player) (expectedScore: float) (actualScore: float) =
+    { player with rating = player.rating + KFactor * (actualScore - expectedScore) }
+
+module WikiExample =
+    // https://en.wikipedia.org/wiki/Elo_rating_system#Theory
+
+    let players = [ { name = "A"; rating = 1613.0 }
+                    { name = "B"; rating = 1609.0 }
+                    { name = "C"; rating = 1477.0 }
+                    { name = "D"; rating = 1388.0 }
+                    { name = "E"; rating = 1586.0 }
+                    { name = "F"; rating = 1720.0 } ]
+
+    let expectedScores =
+        players
+        |> Seq.map (fun p -> expectedScore players.[0] p)
+        |> Seq.zip players
+        |> Seq.skip 1 // drop A
+
+    let expectation: float =
+        expectedScores
+        |> Seq.fold (fun acc (_, s) -> acc + s) 0.0
+
+    let actualScores: float list = [0.0; 0.5; 1.0; 1.0; 0.0]
+
+    let actual: float = Seq.fold (+) 0.0 actualScores
+
+    let updatedRating = updateRating players.[0] actual expectation 
+
+    // updatedRating should be 1601
---
+++ results.fsx
@@ -0,0 +1,57 @@
+#r "System.Xml.Linq.dll"
+
+open System
+open System.Xml.Linq
+open System.Xml
+open System.Xml.XPath
+
+module Ergast =
+    open System
+    open System.Net
+
+    let api = Uri("http://ergast.com/api/f1/")
+
+    let download (uri: Uri) = 
+        let wc = new WebClient()
+        wc.DownloadString uri
+
+    let parseXml (xml: string) =
+        xml.Replace("xmlns", "fuckyouxml")
+        |> XElement.Parse
+        |> XDocument
+
+    let rounds year =
+        Uri(api, sprintf "%i" year)
+        |> download
+        |> parseXml
+
+    let results year round =
+        Uri(api, sprintf "%i/%i/results" year round)
+        |> download
+        |> parseXml
+
+module XPath = 
+    let select (xml: XDocument) (query: string) =
+        xml.XPathSelectElements query
+
+    let selectAttrs (xml: XDocument) (query: string) (attr: string) =
+        select xml query
+        |> Seq.map (fun node -> node.Attribute(XName.Get attr))
+        |> Seq.filter (fun node -> null <> node)
+        |> Seq.map (fun node -> node.Value)
+
+let years = Seq.init 16 (fun i -> 2000 + i)
+
+let rounds = seq { for year in years do
+                   let races = Ergast.rounds year
+                   let rounds = XPath.selectAttrs races "//Race" "round"
+                                |> Seq.map Int32.Parse
+                   yield year, rounds }
+
+let results = seq { for y,rs in rounds do
+                    for r in rs do
+                    let results = Ergast.results y r
+                    yield y, r, results }
+
+results
+|> Seq.iter (fun (y,r,xml) -> xml.Save(sprintf "results_%i_%i.xml" y r))
